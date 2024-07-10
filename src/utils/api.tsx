6b40ffa17086abbf3e24768f27dbd90c97be1e89
Senior Coder <agilecoder2040@gmail.com>
2024-07-11 01:38:47 +0900

    d

--- src/utils/api.tsx
+++ src/utils/api.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const api = axios.create({
-    baseURL: `https://localhost:5000/api`,
+    baseURL: `http://localhost:5000/api`,
     headers: {
       'Content-Type': 'application/json'
     }
